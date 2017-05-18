99442bb0a2df80d55197deb8771d33e24e7ac5f5
JaekelJulian <JaekelJulian@dv-schulen.de>
2017-05-18 15:21:01 +0200

    Created Attribute, Constraints, Entity and Relathinship (#nobrain) classes.

---
+++ DBinDB/Attribute.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBinDB
+{
+    class Attribute
+    {
+        private string name;
+        private bool isPrimary;
+        private bool isForeign;
+        private string type;
+        private string defaultValue;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public bool IsPrimary
+        {
+            get
+            {
+                return isPrimary;
+            }
+
+            set
+            {
+                isPrimary = value;
+            }
+        }
+
+        public bool IsForeign
+        {
+            get
+            {
+                return isForeign;
+            }
+
+            set
+            {
+                isForeign = value;
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+
+            set
+            {
+                type = value;
+            }
+        }
+
+        public string DefaultValue
+        {
+            get
+            {
+                return defaultValue;
+            }
+
+            set
+            {
+                defaultValue = value;
+            }
+        }
+    }
+}
---
+++ DBinDB/Constraints.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBinDB
+{
+    class Constraints
+    {
+        private string name;
+        private string type;
+        private string value;
+        private bool enabled;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+
+            set
+            {
+                type = value;
+            }
+        }
+
+        public string Value
+        {
+            get
+            {
+                return value;
+            }
+
+            set
+            {
+                this.value = value;
+            }
+        }
+
+        public bool Enabled
+        {
+            get
+            {
+                return enabled;
+            }
+
+            set
+            {
+                enabled = value;
+            }
+        }
+    }
+}
